77cc3f4aee5254eb1050fcdc2c5e97f6d97984ce
Viljami Vastamäki <viljami.vastamaki@nordcloud.com>
2020-02-18 14:47:39 +0200

    wibar and rc lua refactoring

--- rc.lua
+++ rc.lua
@@ -23,193 +23,208 @@ beautiful.notification_shape = gears.shape.rounded_rect
 beautiful.notification_bg = "#282a36"
 beautiful.notification_font = "Source Code Pro 9"
 
-awful.layout.layouts = {
-    awful.layout.suit.floating,
-awful.layout.suit.tile, }
+awful.layout.layouts = {awful.layout.suit.floating, awful.layout.suit.tile}
 
 myawesomemenu = {
-    {"manual", terminal .. " -e man awesome", menubar.utils.lookup_icon("system-help")},
-    {"edit config", gui_editor .. " " .. awesome.conffile, menubar.utils.lookup_icon("accessories-text-editor")},
-{"restart", awesome.restart, menubar.utils.lookup_icon("system-restart")}}
+    {
+        "manual", terminal .. " -e man awesome",
+        menubar.utils.lookup_icon("system-help")
+    }, {
+        "edit config", gui_editor .. " " .. awesome.conffile,
+        menubar.utils.lookup_icon("accessories-text-editor")
+    }, {"restart", awesome.restart, menubar.utils.lookup_icon("system-restart")}
+}
 myexitmenu = {
-    {"log out", function() awesome.quit() end, menubar.utils.lookup_icon("system-log-out")},
-    {"suspend", "systemctl suspend", menubar.utils.lookup_icon("system-suspend")},
-    {"hibernate", "systemctl hibernate", menubar.utils.lookup_icon("system-suspend-hibernate")},
+    {
+        "log out", function() awesome.quit() end,
+        menubar.utils.lookup_icon("system-log-out")
+    },
+    {
+        "suspend", "systemctl suspend",
+        menubar.utils.lookup_icon("system-suspend")
+    }, {
+        "hibernate", "systemctl hibernate",
+        menubar.utils.lookup_icon("system-suspend-hibernate")
+    },
     {"reboot", "systemctl reboot", menubar.utils.lookup_icon("system-reboot")},
-{"shutdown", "poweroff", menubar.utils.lookup_icon("system-shutdown")}}
+    {"shutdown", "poweroff", menubar.utils.lookup_icon("system-shutdown")}
+}
 mymainmenu = freedesktop.menu.build({
     icon_size = 32,
     after = {
         {"Awesome", myawesomemenu, "/usr/share/awesome/icons/awesome32.png"},
-        {"Exit", myexitmenu, menubar.utils.lookup_icon("system-shutdown")},
-    }})
-    
-    mylauncher = awful.widget.launcher({image = beautiful.awesome_icon, menu = mymainmenu})
-    
-    root.buttons(gears.table.join(
-        awful.button({}, 1, function () mymainmenu:hide() end),
-        awful.button({}, 3, function () mymainmenu:toggle() end),
-        awful.button({}, 4, awful.tag.viewnext),
-    awful.button({}, 5, awful.tag.viewprev)))
-    
-    -- {{{ Helper functions
-    local function client_menu_toggle_fn()
-        local instance = nil
-        return function ()
-            if instance and instance.wibox.visible then
-                instance:hide()
-                instance = nil
-            else
-                instance = awful.menu.clients({theme = {width = 250}})
-            end
+        {"Exit", myexitmenu, menubar.utils.lookup_icon("system-shutdown")}
+    }
+})
+
+mylauncher = awful.widget.launcher({
+    image = beautiful.awesome_icon,
+    menu = mymainmenu
+})
+
+root.buttons(gears.table.join(awful.button({}, 1,
+                                           function() mymainmenu:hide() end),
+                              awful.button({}, 3,
+                                           function() mymainmenu:toggle() end),
+                              awful.button({}, 4, awful.tag.viewnext),
+                              awful.button({}, 5, awful.tag.viewprev)))
+
+-- {{{ Helper functions
+local function client_menu_toggle_fn()
+    local instance = nil
+    return function()
+        if instance and instance.wibox.visible then
+            instance:hide()
+            instance = nil
+        else
+            instance = awful.menu.clients({theme = {width = 250}})
         end
     end
-    -- }}}
-    
-    local globalkeys, clientkeys, clientbuttons = require("keybinds")()
-    
-    root.keys(globalkeys)
-    -- {{{ Rules
-    -- Rules to apply to new clients (through the "manage" signal).
-    awful.rules.rules = {
-        -- All clients will match this rule.
-        {rule = {},
-            properties = {border_width = beautiful.border_width,
-                border_color = beautiful.border_normal,
-                focus = awful.client.focus.filter,
-                raise = true,
-                keys = clientkeys,
-                buttons = clientbuttons,
-                size_hints_honor = false, -- Remove gaps between terminals
-                screen = awful.screen.preferred,
-                callback = awful.client.setslave,
-                placement = awful.placement.no_overlap + awful.placement.no_offscreen
-            }},
-            
-            -- Floating clients.
-            {rule_any = {
-                instance = {
-                    "DTA", -- Firefox addon DownThemAll.
-                    "copyq", -- Includes session name in class.
-                },
-                class = {
-                    "Arandr",
-                    "Gpick",
-                    "Kruler",
-                    "MessageWin", -- kalarm.
-                    "Sxiv",
-                    "Wpa_gui",
-                    "pinentry",
-                    "veromix",
-                "xtightvncviewer"},
-                
-                name = {
-                    "Event Tester", -- xev.
-                },
-                role = {
-                    "AlarmWindow", -- Thunderbird's calendar.
-                    "pop-up", -- e.g. Google Chrome's (detached) Developer Tools.
-                }}, properties = {floating = true}},
-                
-                -- Add titlebars to normal clients and dialogs
-                {rule_any = {type = {"normal", "dialog"}},
-                properties = {titlebars_enabled = false}},
+end
+-- }}}
+
+local globalkeys, clientkeys, clientbuttons = require("keybinds")()
+
+root.keys(globalkeys)
+-- {{{ Rules
+-- Rules to apply to new clients (through the "manage" signal).
+awful.rules.rules = {
+    -- All clients will match this rule.
+    {
+        rule = {},
+        properties = {
+            border_width = beautiful.border_width,
+            border_color = beautiful.border_normal,
+            focus = awful.client.focus.filter,
+            raise = true,
+            keys = clientkeys,
+            buttons = clientbuttons,
+            size_hints_honor = false, -- Remove gaps between terminals
+            screen = awful.screen.preferred,
+            callback = awful.client.setslave,
+            placement = awful.placement.no_overlap +
+                awful.placement.no_offscreen
+        }
+    }, -- Floating clients.
+    {
+        rule_any = {
+            instance = {
+                "DTA", -- Firefox addon DownThemAll.
+                "copyq" -- Includes session name in class.
+            },
+            class = {
+                "Arandr", "Gpick", "Kruler", "MessageWin", -- kalarm.
+                "Sxiv", "Wpa_gui", "pinentry", "veromix", "xtightvncviewer"
+            },
+
+            name = {
+                "Event Tester" -- xev.
+            },
+            role = {
+                "AlarmWindow", -- Thunderbird's calendar.
+                "pop-up" -- e.g. Google Chrome's (detached) Developer Tools.
             }
-            -- }}}
-            
-            -- {{{ Signals
-            -- Signal function to execute when a new client appears.
-            client.connect_signal("manage", function (c)
-                c.shape = function(cr, w, h)
-                    gears.shape.rounded_rect(cr, w, h, 8)
+        },
+        properties = {floating = true}
+    }, -- Add titlebars to normal clients and dialogs
+    {
+        rule_any = {type = {"normal", "dialog"}},
+        properties = {titlebars_enabled = false}
+    }
+}
+-- }}}
+
+-- {{{ Signals
+-- Signal function to execute when a new client appears.
+client.connect_signal("manage", function(c)
+    c.shape = function(cr, w, h) gears.shape.rounded_rect(cr, w, h, 8) end
+    if awesome.startup and not c.size_hints.user_position and
+        not c.size_hints.program_position then
+        -- Prevent clients from being unreachable after screen count changes.
+        awful.placement.no_offscreen(c)
+    end
+end)
+
+-- Add a titlebar if titlebars_enabled is set to true in the rules.
+client.connect_signal("request::titlebars", function(c)
+    -- buttons for the titlebar
+    local buttons = gears.table.join(awful.button({}, 1, function()
+        client.focus = c
+        c:raise()
+        awful.mouse.client.move(c)
+    end), awful.button({}, 3, function()
+        client.focus = c
+        c:raise()
+        awful.mouse.client.resize(c)
+    end))
+
+    awful.titlebar(c):setup{
+        { -- Left
+            awful.titlebar.widget.iconwidget(c),
+            buttons = buttons,
+            layout = wibox.layout.fixed.horizontal
+        },
+        { -- Middle
+            { -- Title
+                align = "center",
+                widget = awful.titlebar.widget.titlewidget(c)
+            },
+            buttons = buttons,
+            layout = wibox.layout.flex.horizontal
+        },
+        { -- Right
+            awful.titlebar.widget.floatingbutton(c),
+            awful.titlebar.widget.stickybutton(c),
+            -- awful.titlebar.widget.ontopbutton    (c),
+            awful.titlebar.widget.maximizedbutton(c),
+            awful.titlebar.widget.closebutton(c),
+            layout = wibox.layout.fixed.horizontal()
+        },
+        layout = wibox.layout.align.horizontal
+    }
+end)
+
+-- Enable sloppy focus, so that focus follows mouse.
+client.connect_signal("mouse::enter", function(c)
+    if awful.layout.get(c.screen) ~= awful.layout.suit.magnifier and
+        awful.client.focus.filter(c) then client.focus = c end
+end)
+
+client.connect_signal("focus",
+                      function(c) c.border_color = beautiful.border_focus end)
+client.connect_signal("unfocus",
+                      function(c) c.border_color = beautiful.border_normal end)
+
+-- Disable borders on lone windows
+-- Handle border sizes of clients.
+for s = 1, screen.count() do
+    screen[s]:connect_signal("arrange", function()
+        local clients = awful.client.visible(s)
+        local layout = awful.layout.getname(awful.layout.get(s))
+
+        for _, c in pairs(clients) do
+            -- No borders with only one humanly visible client
+            if c.maximized then
+                -- NOTE: also handled in focus, but that does not cover maximizing from a
+                -- tiled state (when the client had focus).
+                c.border_width = 0
+            elseif c.floating or layout == "floating" then
+                c.border_width = beautiful.border_width
+            elseif layout == "max" or layout == "fullscreen" then
+                c.border_width = 0
+            else
+                local tiled = awful.client.tiled(c.screen)
+                if #tiled == 1 then -- and c == tiled[1] then
+                    tiled[1].border_width = 0
+                else
+                    c.border_width = beautiful.border_width
                 end
-                if awesome.startup and
-                    not c.size_hints.user_position
-                    and not c.size_hints.program_position then
-                    -- Prevent clients from being unreachable after screen count changes.
-                    awful.placement.no_offscreen(c)
-                end
-            end)
-            
-            -- Add a titlebar if titlebars_enabled is set to true in the rules.
-            client.connect_signal("request::titlebars", function(c)
-                -- buttons for the titlebar
-                local buttons = gears.table.join(
-                    awful.button({}, 1, function()
-                        client.focus = c
-                        c:raise()
-                        awful.mouse.client.move(c)
-                    end),
-                    awful.button({}, 3, function()
-                        client.focus = c
-                        c:raise()
-                        awful.mouse.client.resize(c)
-                    end))
-                    
-                    awful.titlebar(c) : setup {
-                        {-- Left
-                            awful.titlebar.widget.iconwidget(c),
-                            buttons = buttons,
-                            layout = wibox.layout.fixed.horizontal
-                        },
-                        {-- Middle
-                            {-- Title
-                                align = "center",
-                            widget = awful.titlebar.widget.titlewidget(c)},
-                            buttons = buttons,
-                            layout = wibox.layout.flex.horizontal
-                        },
-                        {-- Right
-                            awful.titlebar.widget.floatingbutton (c),
-                            awful.titlebar.widget.stickybutton (c),
-                            -- awful.titlebar.widget.ontopbutton    (c),
-                            awful.titlebar.widget.maximizedbutton(c),
-                            awful.titlebar.widget.closebutton (c),
-                        layout = wibox.layout.fixed.horizontal()},
-                        layout = wibox.layout.align.horizontal
-                    }
-                end)
-                
-                -- Enable sloppy focus, so that focus follows mouse.
-                client.connect_signal("mouse::enter", function(c)
-                    if awful.layout.get(c.screen) ~= awful.layout.suit.magnifier
-                        and awful.client.focus.filter(c) then
-                        client.focus = c
-                    end
-                end)
-                
-                client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
-                client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
-                
-                -- Disable borders on lone windows
-                -- Handle border sizes of clients.
-                for s = 1, screen.count() do screen[s]:connect_signal("arrange", function ()
-                    local clients = awful.client.visible(s)
-                    local layout = awful.layout.getname(awful.layout.get(s))
-                    
-                    for _, c in pairs(clients) do
-                        -- No borders with only one humanly visible client
-                        if c.maximized then
-                            -- NOTE: also handled in focus, but that does not cover maximizing from a
-                            -- tiled state (when the client had focus).
-                            c.border_width = 0
-                        elseif c.floating or layout == "floating" then
-                            c.border_width = beautiful.border_width
-                        elseif layout == "max" or layout == "fullscreen" then
-                            c.border_width = 0
-                        else
-                            local tiled = awful.client.tiled(c.screen)
-                            if #tiled == 1 then -- and c == tiled[1] then
-                                tiled[1].border_width = 0
-                            else
-                                c.border_width = beautiful.border_width
-                            end
-                        end
-                    end
-                end)
             end
-            
-            -- }}}
-            
-            awful.spawn.with_shell("~/.config/awesome/autorun.sh")
-            
+        end
+    end)
+end
+
+-- }}}
+
+awful.spawn.with_shell("~/.config/awesome/autorun.sh")
+
--- wibar.lua
+++ wibar.lua
@@ -11,118 +11,114 @@ local clock = wibox.widget.textclock(" %d.%m. | %H:%M")
 local calendar = wibox.widget.calendar.month(os.date('*t'))
 calendar.font = font
 
-local memory_usage =
-lain.widget.mem(
-    {
-        settings = function()
-            text = string.format("%sMB/%s", mem_now.used, mem_now.total)
-            widget:set_markup(markup.font(font, " " .. text .. "MB "))
+local memory_usage = lain.widget.mem({
+    settings = function()
+        text = string.format("%sMB/%s", mem_now.used, mem_now.total)
+        widget:set_markup(markup.font(font, " " .. text .. "MB "))
+    end
+})
+local cpu_usage = lain.widget.cpu({
+    settings = function()
+        cores = ""
+        for i = 1, 4, 1 do cores = cores .. cpu_now[i].usage .. "% " end
+        widget:set_markup(markup.font(font, " " .. cores))
+    end
+})
+local battery = lain.widget.bat({
+    settings = function()
+        if bat_now.ac_status == 1 then
+            widget:set_markup(markup.font(font, " AC "))
+            return
         end
+        widget:set_markup(markup.font(font, " Battery: " .. bat_now.perc .. "%"))
+    end
+})
+
+local network = lain.widget.net({
+    settings = function()
+        widget:set_markup(markup.font(font, " " .. net_now.received ..
+                                          " ↓↑ " .. net_now.sent .. ""))
+    end
+})
+local arrow = separators.arrow_left
+
+local widget_colors = {
+    filler = "#282a36",
+    clock = "#ffaac3",
+    net = "#f8b195",
+    temp = "#f67280",
+    cpu = "#c06c84",
+    mem = "#6c567b"
+}
+
+awful.screen.connect_for_each_screen(function(s)
+    awful.tag({"1", "2"}, s, awful.layout.layouts[1])
+    s.layoutbox = awful.widget.layoutbox(s)
+    s.layoutbox:buttons(gears.table.join(
+                            awful.button({}, 1,
+                                         function() awful.layout.inc(1) end),
+                            awful.button({}, 3,
+                                         function() awful.layout.inc(-1) end),
+                            awful.button({}, 4,
+                                         function() awful.layout.inc(1) end),
+                            awful.button({}, 5,
+                                         function() awful.layout.inc(-1) end)))
+    s.taglist = awful.widget.taglist(s, awful.widget.taglist.filter.all)
+
+    s.topbar = awful.wibar({
+        position = "top",
+        screen = s,
+        bg = "#282a36",
+        wibar_shape = gears.shape.rounded_rect
     })
-    local cpu_usage =
-    lain.widget.cpu(
+
+    s.topbar:setup{
+        layout = wibox.layout.align.horizontal,
         {
-            settings = function()
-                cores = ""
-                for i = 1, 4, 1 do
-                    cores = cores .. cpu_now[i].usage .. "% "
-                end
-                widget:set_markup(markup.font(font, " " .. cores))
-            end
-        })
-        local battery =
-        lain.widget.bat(
-            {
-                settings = function()
-                    if bat_now.ac_status == 1 then
-                        widget:set_markup(markup.font(font, " AC "))
-                        return
-                    end
-                    widget:set_markup(markup.font(font, " Battery: " .. bat_now.perc .. "%"))
-                end
-            })
-            
-            local network =
-            lain.widget.net(
-                {
-                    settings = function()
-                        widget:set_markup(markup.font(font, " " .. net_now.received .. " ↓↑ " .. net_now.sent .. ""))
-                    end
-                })
-                local arrow = separators.arrow_left
-                
-                local widget_colors = {
-                    filler = "#282a36",
-                    clock = "#ffaac3",
-                    net = "#f8b195",
-                    temp = "#f67280",
-                    cpu = "#c06c84",
-                    mem = "#6c567b",
-                }
-                
-                awful.screen.connect_for_each_screen(function(s)
-                    awful.tag({"1", "2"}, s, awful.layout.layouts[1])
-                    s.layoutbox = awful.widget.layoutbox(s)
-                    s.layoutbox:buttons(gears.table.join(
-                        awful.button({}, 1, function () awful.layout.inc(1) end),
-                        awful.button({}, 3, function () awful.layout.inc(-1) end),
-                        awful.button({}, 4, function () awful.layout.inc(1) end),
-                    awful.button({}, 5, function () awful.layout.inc(-1) end)))
-                    s.taglist = awful.widget.taglist(s, awful.widget.taglist.filter.all)
-                    
-                    s.topbar = awful.wibar({position = "top", screen = s, bg = "#282a36", wibar_shape = gears.shape.rounded_rect})
-                    
-                    s.topbar:setup {
-                        layout = wibox.layout.align.horizontal,
-                        {
-                            layout = wibox.layout.fixed.horizontal,
-                            s.taglist,
-                            wibox.widget.systray(),
-                        },
-                        nil,
-                        {
-                            layout = wibox.layout.fixed.horizontal,
-                            arrow(alpha, widget_colors.filler),
-                            arrow(widget_colors.filler, widget_colors.clock),
-                            wibox.container.background(
-                                wibox.container.margin(
-                                    wibox.widget {_, network.widget, layout = wibox.layout.align.horizontal},
-                                    dpi(2),
-                                dpi(3)),
-                                widget_colors.clock
-                            ),
-                            arrow(widget_colors.clock, widget_colors.net),
-                            wibox.container.background(
-                                wibox.container.margin(
-                                    wibox.widget {_, battery.widget, layout = wibox.layout.align.horizontal},
-                                    dpi(2),
-                                dpi(3)),
-                                widget_colors.net
-                            ),
-                            arrow(widget_colors.net, widget_colors.temp),
-                            wibox.container.background(
-                                wibox.container.margin(
-                                    wibox.widget {_, cpu_usage.widget, layout = wibox.layout.align.horizontal},
-                                    dpi(2),
-                                dpi(3)),
-                                widget_colors.temp
-                            ), arrow(widget_colors.temp, widget_colors.cpu),
-                            wibox.container.background(
-                                wibox.container.margin(
-                                    wibox.widget {_, memory_usage.widget, layout = wibox.layout.align.horizontal},
-                                    dpi(2),
-                                dpi(3)),
-                                widget_colors.cpu
-                            ),
-                            arrow(widget_colors.cpu, widget_colors.clock),
-                            wibox.container.background(
-                                wibox.container.margin(
-                                    wibox.widget {_, clock, layout = wibox.layout.align.horizontal},
-                                    dpi(2),
-                                dpi(3)),
-                                widget_colors.clock
-                            ),
-                        },
-                    }
-                end)
-                
+            layout = wibox.layout.fixed.horizontal,
+            s.taglist,
+            wibox.widget.systray()
+        },
+        nil,
+        {
+            layout = wibox.layout.fixed.horizontal,
+            arrow(alpha, widget_colors.filler),
+            arrow(widget_colors.filler, widget_colors.clock),
+            wibox.container.background(wibox.container.margin(
+                                           wibox.widget {
+                    _,
+                    network.widget,
+                    layout = wibox.layout.align.horizontal
+                }, dpi(2), dpi(3)), widget_colors.clock),
+            arrow(widget_colors.clock, widget_colors.net),
+            wibox.container.background(wibox.container.margin(
+                                           wibox.widget {
+                    _,
+                    battery.widget,
+                    layout = wibox.layout.align.horizontal
+                }, dpi(2), dpi(3)), widget_colors.net),
+            arrow(widget_colors.net, widget_colors.temp),
+            wibox.container.background(wibox.container.margin(
+                                           wibox.widget {
+                    _,
+                    cpu_usage.widget,
+                    layout = wibox.layout.align.horizontal
+                }, dpi(2), dpi(3)), widget_colors.temp),
+            arrow(widget_colors.temp, widget_colors.cpu),
+            wibox.container.background(wibox.container.margin(
+                                           wibox.widget {
+                    _,
+                    memory_usage.widget,
+                    layout = wibox.layout.align.horizontal
+                }, dpi(2), dpi(3)), widget_colors.cpu),
+            arrow(widget_colors.cpu, widget_colors.clock),
+            wibox.container.background(wibox.container.margin(
+                                           wibox.widget {
+                    _,
+                    clock,
+                    layout = wibox.layout.align.horizontal
+                }, dpi(2), dpi(3)), widget_colors.clock)
+        }
+    }
+end)
+
